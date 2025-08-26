3727408ff90bd5dd02a90c4556ff99a660b9ac26
asym1 <asym17182@gmail.com>
2025-08-26 13:30:28 +0400

    Adding data retrieval scripts for eeg signal & ap/probability data

---
+++ src/python/scripts/ap_probability_retrieval.py
@@ -0,0 +1,9 @@
+# all necessary imports for data retrieval and storage
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from neurosity import NeurositySDK
+from dotenv import load_dotenv
+import os
+
